70ca96312c96cb6dc4dd011ffb97c69342cb5700
unknown <cheryl@pcschool.com.tw>
2023-11-13 15:27:25 +0800

    new add index.html

---
+++ 2021/js/pcschool-pageSetting.js
@@ -0,0 +1,488 @@
+
+
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 550) {
+        $('.socialBtn').fadeIn();
+    } else {
+        $('.socialBtn').fadeOut();
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
+
+// Slick Setting
+$('.slick-indexAD .slick-wrapper').slick({
+    infinite: true,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            dots: true,
+            arrows: false,
+        }
+    },]
+});
+
+$('.slick-hotClass .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.slick-hotClass .slick-wrapper').slick('setPosition');
+})
+
+$('.slick-it360Class .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            arrows: false,
+			infinite: false,
+            dots: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.slick-videoLearnClass .slick-wrapper').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+    autoplay: true,
+});
+
+$('.slick-latestNewsClass .slick-wrapper').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+    autoplay: true,
+});
+
+$('.slick-seminarClass .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            arrows: false,
+            dots: false,
+			infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.slick-pcschoolTeachersClass .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            arrows: false,
+            dots: false,
+			infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.slick-teachingChannel .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            arrows: false,
+            dots: false,
+			infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.slick-schoolPhoto .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.slick-pageAD .slick-wrapper').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            dots: false,
+        }
+    },]
+});
+
+$('.slick-teachList .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            dots: true,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.slick-moreTeachers .slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    responsive: [{
+        breakpoint: 1366,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: false
+        }
+    },
+    {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            dots: false,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            dots: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            dots: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
+
+$('.liveCoursesClassBox01 .slick-wrapper').slick({
+    slidesToShow: 6,
+    settings: 'unslick',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+			/*arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 8000,
+            dots: true*/
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 8000,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+			dots: true
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 8000,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+            arrows: false,
+            dots: false,
+			infinite: false,
+            autoplay: true,
+            autoplaySpeed: 8000,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    },
+    ]
+});
---
+++ 2021/js/pcschool-webbase.js
@@ -0,0 +1,88 @@
+// Nav
+$(document).ready(function () {
+    // executes when HTML-Document is loaded and DOM is ready
+
+    // breakpoint and up  
+    $(window).resize(function () {
+        if ($(window).width() >= 980) {
+
+            // when you hover a toggle show its dropdown menu
+            $(".navbar .dropdown-toggle").hover(function () {
+                $(this).parent().toggleClass("show");
+                $(this).parent().find(".dropdown-menu").toggleClass("show");
+            });
+
+            // hide the menu when the mouse leaves the dropdown
+            $(".navbar .dropdown-menu").mouseleave(function () {
+                $(this).removeClass("show");
+            });
+
+            // do something here
+        }
+    });
+    // document ready  
+});
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    window.addEventListener('scroll', function () {
+//        if (window.scrollY > 50) {
+//            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+//            navbar_height = document.querySelector('.navbar').offsetHeight;
+//            document.body.style.paddingTop = navbar_height + 'px';
+//        } else {
+//            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+//            document.body.style.paddingTop = '0';
+//        }
+//    });
+//});
+
+// Tools Panel
+$(document).ready(function () {
+    $(".searchBtn").click(function () {
+        $(".searchPanel").slideToggle();
+    });
+});
+
+
+// Footer
+function footerResize() {
+    if ($(window).width() < 1000) {
+        $('.footer-pcschool .footer-list-box ul').hide();
+        $(".footer-list-box h2 i").removeClass('fas fa-angle-up');
+        $(".footer-list-box h2 i").addClass('fas fa-angle-down');
+        $(".footer-list-box h2").click(function () {
+            $(this).siblings().toggle();
+            if ($(this).find('i').hasClass('fa-angle-down')) {
+                $(this).find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+            } else if ($(this).find('i').hasClass('fa-angle-up')) {
+                $(this).find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+            }
+        });
+    } else {
+        $(".footer-list-box h2 i").removeClass('fas fa-angle-up');
+        $(".footer-list-box h2 i").removeClass('fas fa-angle-down');
+        $(".footer-list-box h2").unbind('click');
+        $('.footer-pcschool .footer-list-box ul').show();
+    }
+};
+footerResize();
+$(window).resize(function () {
+    footerResize();
+});
+
+
+// Scroll to Top
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('.goTop').fadeIn(200); // Fade in the arrow
+    } else {
+        $('.goTop').fadeOut(200); // Else fade out the arrow
+    }
+});
+$('.goTop').click(function () { // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
+});
