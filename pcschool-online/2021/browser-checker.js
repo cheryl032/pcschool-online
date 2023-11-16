863e6a07109ad01ab7ee7145f61d2c262ea96126
unknown <cheryl@pcschool.com.tw>
2023-11-16 11:52:39 +0800

    delete unuse index files

--- pcschool-online/2021/browser-checker.js
+++
@@ -1,4 +0,0 @@
-if (window.navigator.userAgent.indexOf("MSIE ") > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-    alert('親愛的同學您好，您使用的瀏覽器為IE，目前本網站已不支援IE，請使用其他瀏覽器。');
-    location.href = 'https://www.google.com.tw/intl/zh-TW/chrome/';
-}
